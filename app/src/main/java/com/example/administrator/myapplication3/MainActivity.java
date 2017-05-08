a7532f23a2a1e518d1a5b90a75b032c07b4cc7da
zhoushen <zhoushen0817@126.com>
2017-05-08 17:33:28 +0800

    zhoushen add

--- app/src/main/java/com/example/administrator/myapplication3/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication3/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.administrator.myapplication3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +15,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView() {
+        //zhoushen add
+        Log.d("zhoushen","log test!!!");
     }
 }
