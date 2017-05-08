195e78dbf8dc7c969770b66bb437046d5c9ac0b7
liaosican@roadrover.cn <liaosican@roadrover.cn>
2017-05-08 17:20:19 +0800

    test

--- app/src/main/java/com/example/administrator/myapplication3/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication3/MainActivity.java
@@ -10,6 +10,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ///////
+        initView();
+    }
+
+    private void initView() {
     }
 }
