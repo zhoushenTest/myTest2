27f56f93a671cb337172266ac8911d195f955143
张超 <zhang0194@126.com>
2017-05-08 17:27:01 +0800

    创建基础类

---
+++ app/src/main/java/com/example/administrator/myapplication3/BaseActivity.java
@@ -0,0 +1,11 @@
+package com.example.administrator.myapplication3;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2017/5/8.
+ * 基础activity
+ */
+
+public class BaseActivity extends AppCompatActivity {
+}
